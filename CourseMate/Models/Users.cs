1c605aee706865096b9dc29a4bc656f72f3398e5
abtaamim <abdullahaltamim001@gmail.com>
2025-09-14 21:38:46 +0600

    save post

--- CourseMate/Models/Users.cs
+++ CourseMate/Models/Users.cs
@@ -2,10 +2,13 @@
 
 namespace CourseMate.Models
 {
-    public class Users : IdentityUser
-    {
-        public string FullName { get; set; }
-        public string Bio { get; set; }  
-        public string ProfileImageUrl { get; set; } 
-    }
+  public class Users : IdentityUser
+  {
+    public string FullName { get; set; }
+    public string Bio { get; set; }
+    public string ProfileImageUrl { get; set; }
+
+    public virtual ICollection<SavedPost> SavedPosts { get; set; } = new List<SavedPost>();
+
+  }
 }
